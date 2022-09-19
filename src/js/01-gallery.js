06e459a252c75ecdf998c5898846995d069f9934
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2022-09-19 22:29:59 +0200

    done first task

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalerryItemsMarkup(galleryItems);
+
+galleryEl.innerHTML = galleryItemsMarkup;
+
+function createGalerryItemsMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <li>
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+      `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
